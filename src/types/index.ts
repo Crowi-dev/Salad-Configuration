066296235e97bbeb0662540a0f939e8a8f021a2b
Teemu <e2402257@edu.vamk.fi>
2026-03-20 14:56:18 +0200

    Added category and ingredients with inheritance

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,3 +11,13 @@ export interface Bowl extends BaseType {
     slot_count: number;
     shape: 'round' | 'square';
 }
+export interface Category {
+    id: number;
+    name: String;
+    base_type_id?: number;
+}
+export interface Ingredient extends BaseType {
+    categoryId: number;
+    diets: String[];
+    weight_grams?: number;
+}
