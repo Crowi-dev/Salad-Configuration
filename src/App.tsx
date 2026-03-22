5ba475f3f5dfe8c46cc95f56e3ff6fc935dc85d4
e2403108 <e2403108@vamk.fi>
2026-03-22 17:54:34 +0200

    feat: create BowlSelection UI shell and add to App (Task 1.8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
 import Header from "./components/Header";
+import BowlSelection from "./components/BowlSelection";
+
 function App() {
-  return(
-    <div>
+  return (
+    <div className="min-h-screen bg-zinc-900">
       <Header />
+      <div className="p-8">
+        <BowlSelection />
+      </div>
     </div>
   );
 }
+
 export default App;
