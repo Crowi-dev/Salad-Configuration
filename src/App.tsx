aa554e033225e2b0c31aad42142bd66631a448e7
Robin <e2402661@edu.vamk.fi>
2026-03-27 09:25:58 +0200

    Fixing things MAYBE

--- src/App.tsx
+++ src/App.tsx
@@ -16,3 +16,29 @@ function App() {
 }
 
 export default App;
+
+
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col bg-white font-sans">
+      
+      {/* Header at top */}
+      <Header />
+
+      {/* Main content */}
+      <main className="flex-1 max-w-6xl w-full mx-auto p-6 flex flex-col gap-8 mt-4">
+        {/* Your page content goes here */}
+      </main>
+
+      {/* Footer at bottom */}
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
