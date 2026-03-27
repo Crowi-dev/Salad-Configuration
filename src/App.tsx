04d7c22f79c770e3306c412361b888ed2a674ef7
e2403108 <e2403108@vamk.fi>
2026-03-27 09:38:04 +0200

    APPTESTI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import Header from "./components/Header";
-import BowlSelection from "./components/BowlSelection";
-import BaseSelection from "./components/BaseSelection";
+import Header from "./Components/Header";
+import BowlSelection from "./Components/BowlSelection";
+import BaseSelection from "./Components/BaseSelection";
+import CenterBowl from "./Components/CenterBowl"; 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="p-8 flex flex-col lg:flex-row gap-8 justify-center items-start">
         <BowlSelection />
         <BaseSelection /> 
+        <CenterBowl />
       </div>
     </div>
   );
