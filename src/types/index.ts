a816724e5c8d6393068e90e75adac187ab037be8
Teemu <e2402257@edu.vamk.fi>
2026-03-31 22:58:40 +0300

    task-2-9-done

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,53 +1,50 @@
 export interface BaseType {
-    id: number;
-    name: String;
-    price?: number;
-    image_url?: String;
-    barcode_url?: String;
+  id: number;
+  name: string;
+  price?: number;
+  image_url?: string;
+  barcode_url?: string;
 }
+
 export interface Bowl extends BaseType {
-    base_type_id?: number;
-    volume?: number;
-    slot_count: number;
-    shape: 'round' | 'square';
+  base_type_id?: number;
+  volume?: number;
+  slot_count: number;
+  shape: 'round' | 'square';
 }
+
 export interface Category {
-    id: number;
-    name: String;
-    base_type_id?: number;
+  id: number;
+  name: string;
+  base_type_id?: number;
 }
+
 export interface Ingredient extends BaseType {
-    categoryId: number;
-    diets: String[];
-    weight_grams?: number;
+  categoryId: number;
+  diets: string[];
+  weight_grams?: number;
 }
+
 export interface User {
-    id: number;
-    email: String;
-    name?: String;
-    role?: String;
+  id: number;
+  email: string;
+  name?: string;
+  role?: string;
 }
+
 export interface Recipe {
-    id: number;
-    userId: number;
-    name: String;
-    bowlId: number;
-    ingredientIds: number[];
-    slots?: Record<string, Ingredient | null>;
-    is_public?: boolean;
+  id: number;
+  userId: number;
+  name: string;
+  bowlId: number;
+  ingredientIds: number[];
+  slots?: Record<string, Ingredient | null>;
+  is_public?: boolean;
 }
+
 export interface PriceListItem {
-    id: number;
-    item_id: number;
-    price: number;
-    type?: String;
-}
-export interface Bowl{
-    /*...*/
-}
-export interface Category{
-    /*...*/
-}
-export interface Ingredient{
-    /*...*/
+  id: number;
+  item_id: number;
+  price: number;
+  type?: string;
 }
